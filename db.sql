46d40043d5da2d01197c89504f9dd5478f181eaf
civic <tsskmail@gmail.com>
2017-05-27 18:01:10 +0900

    dept table add
     Date:      Sat May 27 18:01:10 2017 +0900

--- db.sql
+++ db.sql
@@ -1,11 +1,22 @@
+DROP TABLE IF EXISTS dept CASCADE;
 DROP TABLE IF EXISTS emp CASCADE;
+CREATE TABLE dept(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    dname VARCHAR
+);
 CREATE TABLE emp(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     ename VARCHAR,
-    hiredate DATE
+    hiredate DATE,
+    deptno INTEGER,
+    CONSTRAINT fk_deptno FOREIGN KEY(deptno) REFERENCES dept(id)
 );
-INSERT INTO emp(ename, hiredate) VALUES('Scott', '2017-01-02');
-INSERT INTO emp(ename, hiredate) VALUES('Tiger', '2017-01-12');
-INSERT INTO emp(ename, hiredate) VALUES('John', '2017-01-11');
-INSERT INTO emp(ename, hiredate) VALUES('Bob', '2017-04-13');
-INSERT INTO emp(ename, hiredate) VALUES('Eli', '2017-04-11');
+INSERT INTO dept(id, dname) VALUES(100, 'MANAGER');
+INSERT INTO dept(id, dname) VALUES(200, 'SALESMAN');
+INSERT INTO dept(id, dname) VALUES(300, 'ENGINEER');
+
+INSERT INTO emp(ename, hiredate, deptno) VALUES('Scott', '2017-01-02', 100);
+INSERT INTO emp(ename, hiredate, deptno) VALUES('Tiger', '2017-01-12', 100);
+INSERT INTO emp(ename, hiredate, deptno) VALUES('John', '2017-01-11', 200);
+INSERT INTO emp(ename, hiredate, deptno) VALUES('Bob', '2017-04-13', 200);
+INSERT INTO emp(ename, hiredate, deptno) VALUES('Eli', '2017-04-11', 300);
